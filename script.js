44695c1e7291903929d2bb917c8e777ca7b73db5
rehmanhaider0 <112751420+rehmanhaider0@users.noreply.github.com>
2022-09-03 01:54:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function aa() {
+    let x = document.getElementById("result-menu-icon")
+    if (x.style.height === "190px"){
+        x.style.height = "0px"
+    }
+    else {
+        x.style.height = "190px"
+    }
+}
+function bb() {
+    let x = document.getElementById("result-menu-icon-mini-width")
+    if (x.style.height === "121px"){
+        x.style.height = "0px"
+    }
+    else {
+        x.style.height = "121px"
+    }
+}
+
